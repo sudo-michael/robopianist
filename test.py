929d2ce7792c0bb5ea0ceac6295bbce00e009c09
Michael Lu <lu.michael@outlook.com>
2023-07-23 03:21:38 -0700

    monitor f1 and whatever else

--- test.py
+++ test.py
@@ -6,15 +6,20 @@ import numpy as np
 from robopianist.suite.tasks import self_actuated_piano
 from robopianist.suite.tasks import piano_with_shadow_hands
 from dm_env_wrappers import CanonicalSpecWrapper
-from robopianist.wrappers import PianoSoundVideoWrapper
+from robopianist.wrappers import PianoSoundVideoWrapper, MidiEvaluationWrapper
 from robopianist import music
 from mujoco_utils import composer_utils
 import dm_env
 from dmcgym import DMCGYM
+import shimmy
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.logger import configure
+
 
 def make_env(song: str = 'TwinkleTwinkleRousseau', 
              seed: int = 0,
-             sound: bool = False,):
+             sound: bool = False,
+             log_dir='./logs'):
     """
     Utility function for multiprocessed env.
     :param song: the name of the song
@@ -41,16 +46,19 @@ def make_env(song: str = 'TwinkleTwinkleRousseau',
             task=task, strip_singleton_obs_buffer_dim=True, recompile_physics=False
         )
 
-        if sound:
-            env = PianoSoundVideoWrapper(
-                env,
-                record_every=1,
-                camera_id=None, # "piano/back",
-                record_dir="./videos",
-            )
+        # if sound:
+        #     env = PianoSoundVideoWrapper(
+        #         env,
+        #         record_every=100,
+        #         camera_id=None, # "piano/back",
+        #         record_dir="./videos",
+        #     )
         env = CanonicalSpecWrapper(env)
-
+        # NOTE: modify DMCGYM to return music metrics
+        env = MidiEvaluationWrapper(env)
         env = DMCGYM(env)
+        env = shimmy.GymV21CompatibilityV0(env=env)
+        env = Monitor(env, filename='test_monitor', info_keywords=('precision', 'recall', 'f1', 'sustain_precision', 'sustain_recall', 'sustain_f1'))
 
         return env
 
@@ -60,4 +68,14 @@ def make_env(song: str = 'TwinkleTwinkleRousseau',
 
 if __name__ == '__main__':
     env = make_env('TwinkleTwinkleRousseau', 0, False)()
-    breakpoint()
+
+    from stable_baselines3 import PPO
+
+    tmp_path = "./logs/sb3_log/"
+    # set up logger
+    new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+
+    model = PPO("MultiInputPolicy", env, verbose=1)
+    model.set_logger(new_logger)
+    model.learn(total_timesteps=1_000_000, progress_bar=True)
+    model.save('twinkle_test_2')
